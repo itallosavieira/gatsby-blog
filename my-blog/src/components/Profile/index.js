f94c93ccf78d5a73d633833253a78843a0b3a1b7
itallosavieira <itallo.sa.vieira@gmail.com>
2021-01-07 16:15:19 -0300

    Add Component Profile

---
+++ my-blog/src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1> Itallo Sá </h1>
+        <h2> Front-End Developer </h2>
+        <p> I'm 25 years old, based in São Paulo. I worked as a developer in 2017 followed by 3 years on the entrepreneurship journey - an experience that taught me a lot - and now I'm back. I'm motivated by challenges and I've thirst for knowledge. </p>
+    </div>
+)
+
+export default Profile
--- my-blog/src/pages/index.js
+++ my-blog/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,7 +7,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>My Blog | Itallo Sá</h1>
+    <>
+      <h1>My Blog | Itallo Sá</h1>
+      <ul>
+        <li>
+          <Link to='/' activeStyle={{ color: 'red' }}> Home </Link>
+        </li>
+        <li>
+          <Link to='/about' activeStyle={{ color: 'red' }}> About </Link>
+        </li>
+      </ul>
+    </>
   </Layout>
 )
 
